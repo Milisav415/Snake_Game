75195441e688ea0e22d83bbbfe047467145415d9
Milisav415 <milisav.jovanovic415@gmail.com>
2024-12-19 14:47:41 +0100

    Add star/reset screen

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -8,17 +8,24 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
 public class GameBoard extends JPanel implements ActionListener {
 
-    private final int BOARD_WIDTH = 300;
-    private final int BOARD_HEIGHT = 300;
+    private enum GameState {
+        START_MENU,
+        IN_GAME,
+        GAME_OVER
+    }
+
+    private final int BOARD_WIDTH = 500;
+    private final int BOARD_HEIGHT = 500;
     private final int DOT_SIZE = 10;    // Size of each snake segment and the food
     private final int ALL_DOTS = (BOARD_WIDTH * BOARD_HEIGHT) / (DOT_SIZE * DOT_SIZE);
-    private final int RAND_POS = 29;    // For randomizing food position (given DOT_SIZE=10 and 300/10=30 cells)
-    private final int DELAY = 140;      // The speed of the game (ms between ticks)
+    private final int RAND_POS = 29;
+    private final int DELAY = 140;
 
     private final int x[] = new int[ALL_DOTS];
     private final int y[] = new int[ALL_DOTS];
@@ -31,7 +38,8 @@ public class GameBoard extends JPanel implements ActionListener {
     private boolean rightDirection = true;
     private boolean upDirection = false;
     private boolean downDirection = false;
-    private boolean inGame = true;
+
+    private GameState gameState = GameState.START_MENU;
 
     private Timer timer;
 
@@ -44,7 +52,6 @@ public class GameBoard extends JPanel implements ActionListener {
         setPreferredSize(new Dimension(BOARD_WIDTH, BOARD_HEIGHT));
         setFocusable(true);
         addKeyListener(new TAdapter());
-        startGame();
     }
 
     private void startGame() {
@@ -56,56 +63,94 @@ public class GameBoard extends JPanel implements ActionListener {
             y[z] = 50;
         }
 
-        locateFood(); // Place the first food
+        locateFood();
+        leftDirection = false;
+        rightDirection = true;
+        upDirection = false;
+        downDirection = false;
+
         timer = new Timer(DELAY, this);
         timer.start();
+        gameState = GameState.IN_GAME;
+    }
+
+    private void endGame() {
+        gameState = GameState.GAME_OVER;
+        if (timer != null) {
+            timer.stop();
+        }
     }
 
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        doDrawing(g);
-    }
-
-    private void doDrawing(Graphics g) {
-        if (inGame) {
-            // Draw the food
-            g.setColor(Color.red);
-            g.fillRect(food_x, food_y, DOT_SIZE, DOT_SIZE);
-
-            // Draw the snake
-            for (int z = 0; z < dots; z++) {
-                if (z == 0) {
-                    // Head of the snake
-                    g.setColor(Color.green);
-                    g.fillRect(x[z], y[z], DOT_SIZE, DOT_SIZE);
-                } else {
-                    // Body of the snake
-                    g.setColor(Color.yellow);
-                    g.fillRect(x[z], y[z], DOT_SIZE, DOT_SIZE);
-                }
-            }
-
-            Toolkit.getDefaultToolkit().sync();
-        } else {
-            gameOver(g);
+        switch (gameState) {
+            case START_MENU:
+                drawStartMenu(g);
+                break;
+            case IN_GAME:
+                doDrawing(g);
+                break;
+            case GAME_OVER:
+                drawGameOver(g);
+                break;
         }
     }
 
-    private void gameOver(Graphics g) {
+    private void doDrawing(Graphics g) {
+        // Draw the food
+        g.setColor(Color.red);
+        g.fillRect(food_x, food_y, DOT_SIZE, DOT_SIZE);
+
+        // Draw the snake
+        for (int z = 0; z < dots; z++) {
+            if (z == 0) {
+                // Head of the snake
+                g.setColor(Color.green);
+                g.fillRect(x[z], y[z], DOT_SIZE, DOT_SIZE);
+            } else {
+                // Body of the snake
+                g.setColor(Color.yellow);
+                g.fillRect(x[z], y[z], DOT_SIZE, DOT_SIZE);
+            }
+        }
+
+        Toolkit.getDefaultToolkit().sync();
+    }
+
+    private void drawStartMenu(Graphics g) {
+        String title = "SNAKE GAME";
+        String prompt = "Press ENTER to start";
+
+        g.setColor(Color.white);
+        Font large = new Font("Helvetica", Font.BOLD, 20);
+        FontMetrics fm = getFontMetrics(large);
+        g.setFont(large);
+
+        g.drawString(title, (BOARD_WIDTH - fm.stringWidth(title)) / 2, BOARD_HEIGHT / 2 - 30);
+
+        Font smaller = new Font("Helvetica", Font.PLAIN, 14);
+        g.setFont(smaller);
+        fm = getFontMetrics(smaller);
+        g.drawString(prompt, (BOARD_WIDTH - fm.stringWidth(prompt)) / 2, BOARD_HEIGHT / 2);
+    }
+
+    private void drawGameOver(Graphics g) {
         String msg = "Game Over";
         String scoreMsg = "Score: " + (dots - 3);
+        String restartMsg = "Press ENTER to restart";
+
         Font small = new Font("Helvetica", Font.BOLD, 14);
         FontMetrics metr = getFontMetrics(small);
 
         g.setColor(Color.white);
         g.setFont(small);
-        g.drawString(msg, (BOARD_WIDTH - metr.stringWidth(msg)) / 2, BOARD_HEIGHT / 2);
-        g.drawString(scoreMsg, (BOARD_WIDTH - metr.stringWidth(scoreMsg)) / 2, (BOARD_HEIGHT / 2) + 20);
+        g.drawString(msg, (BOARD_WIDTH - metr.stringWidth(msg)) / 2, BOARD_HEIGHT / 2 - 20);
+        g.drawString(scoreMsg, (BOARD_WIDTH - metr.stringWidth(scoreMsg)) / 2, BOARD_HEIGHT / 2);
+        g.drawString(restartMsg, (BOARD_WIDTH - metr.stringWidth(restartMsg)) / 2, BOARD_HEIGHT / 2 + 20);
     }
 
     private void checkFood() {
-        // If snake head is on the food
         if ((x[0] == food_x) && (y[0] == food_y)) {
             dots++;
             locateFood();
@@ -123,15 +168,12 @@ public class GameBoard extends JPanel implements ActionListener {
         if (leftDirection) {
             x[0] -= DOT_SIZE;
         }
-
         if (rightDirection) {
             x[0] += DOT_SIZE;
         }
-
         if (upDirection) {
             y[0] -= DOT_SIZE;
         }
-
         if (downDirection) {
             y[0] += DOT_SIZE;
         }
@@ -141,32 +183,13 @@ public class GameBoard extends JPanel implements ActionListener {
         // Check if the snake hits its own body
         for (int z = dots - 1; z > 0; z--) {
             if ((z > 4) && (x[0] == x[z]) && (y[0] == y[z])) {
-                inGame = false;
+                endGame();
             }
         }
 
-        // Check if the snake hits the left border
-        if (x[0] < 0) {
-            inGame = false;
-        }
-
-        // Check if the snake hits the right border
-        if (x[0] >= BOARD_WIDTH) {
-            inGame = false;
-        }
-
-        // Check if the snake hits the top border
-        if (y[0] < 0) {
-            inGame = false;
-        }
-
-        // Check if the snake hits the bottom border
-        if (y[0] >= BOARD_HEIGHT) {
-            inGame = false;
-        }
-
-        if (!inGame) {
-            timer.stop();
+        // Check walls
+        if (x[0] < 0 || x[0] >= BOARD_WIDTH || y[0] < 0 || y[0] >= BOARD_HEIGHT) {
+            endGame();
         }
     }
 
@@ -180,12 +203,11 @@ public class GameBoard extends JPanel implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (inGame) {
+        if (gameState == GameState.IN_GAME) {
             checkFood();
             move();
             checkCollision();
         }
-
         repaint();
     }
 
@@ -196,28 +218,39 @@ public class GameBoard extends JPanel implements ActionListener {
 
             int key = e.getKeyCode();
 
-            if ((key == KeyEvent.VK_LEFT) && (!rightDirection)) {
-                leftDirection = true;
-                upDirection = false;
-                downDirection = false;
-            }
+            if (gameState == GameState.START_MENU) {
+                if (key == KeyEvent.VK_ENTER) {
+                    startGame();
+                }
+            } else if (gameState == GameState.IN_GAME) {
+                if ((key == KeyEvent.VK_LEFT) && (!rightDirection)) {
+                    leftDirection = true;
+                    upDirection = false;
+                    downDirection = false;
+                }
 
-            if ((key == KeyEvent.VK_RIGHT) && (!leftDirection)) {
-                rightDirection = true;
-                upDirection = false;
-                downDirection = false;
-            }
+                if ((key == KeyEvent.VK_RIGHT) && (!leftDirection)) {
+                    rightDirection = true;
+                    upDirection = false;
+                    downDirection = false;
+                }
 
-            if ((key == KeyEvent.VK_UP) && (!downDirection)) {
-                upDirection = true;
-                rightDirection = false;
-                leftDirection = false;
-            }
+                if ((key == KeyEvent.VK_UP) && (!downDirection)) {
+                    upDirection = true;
+                    rightDirection = false;
+                    leftDirection = false;
+                }
 
-            if ((key == KeyEvent.VK_DOWN) && (!upDirection)) {
-                downDirection = true;
-                rightDirection = false;
-                leftDirection = false;
+                if ((key == KeyEvent.VK_DOWN) && (!upDirection)) {
+                    downDirection = true;
+                    rightDirection = false;
+                    leftDirection = false;
+                }
+            } else if (gameState == GameState.GAME_OVER) {
+                if (key == KeyEvent.VK_ENTER) {
+                    // Restart the game by calling startGame again
+                    startGame();
+                }
             }
         }
     }
